1f9f6a32305debec2b456f4bff17516392a7fc88
bet-gregori <elisabet.gregori@gmail.com>
2017-08-03 09:59:58 +0200

    Updated SDFhelper functionalities

--- src/SDFhelper.py
+++ src/SDFhelper.py
@@ -3,32 +3,20 @@
 import os, sys, argparse, re
 from rdkit import Chem
 
-def getPropertyValue(mol, prop):
-    l = [i for i in mol.GetPropNames()]
-    if not cmpdID in l: continue
-
-    print (cmpdID)
-    print (mol.GetPropNames()[l.index(cmpdID)])
-    sys.exit()
-    val = mol.GetProp(mol.GetPropNames()[l.index(cmpdID)])
-    
-    return val
-
 def getName(mol):
     return mol.GetProp("_Name")
-
-def writeProperties(fh, propD):
-    for prop in propD:
-        fh.write('>  <'+prop+'>\n'+propD[prop]+'\n\n') 
     
 def setName(mol, ID):
     mol.SetProp("_Name", ID)
     return mol
 
-def writeMol(mol, fh, propD, ID=None):
+def writeProperties(fh, propD):
+    for prop in propD:
+        fh.write('>  <{}>\n{}\n\n'.format(prop, propD[prop])) 
+
+def writeSDF(mol, fh, propD, ID=None):
     if ID:
         mol = setName(mol, ID)
-    mb = Chem.MolToMolBlock(mol)
-    fh.write(mb)
+    fh.write(Chem.MolToMolBlock(mol))
     writeProperties(fh, propD)
     fh.write('$$$$\n')
