451c5477f76eed96e2757e22e8736ac36bb72ded
BielStela <biel.stela@gmail.com>
2018-02-14 16:17:10 +0100

    Added haders with author info and licence

--- phitools/moleculeHelper.py
+++ phitools/moleculeHelper.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##    Description    Add Data to SDFile
+##                   
+##    Authors:       Inés Martínez (mmartinez4@imim.es)
+##                   Manuel Pastor (manuel.pastor@upf.edu)
+##
+##    Copyright 2015 Manuel Pastor
+##
+##    This file is part of PhiTools
+##
+##    PhiTools is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation version 3.
+##
+##    PhiTools is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with PhiTools.  If not, see <http://www.gnu.org/licenses/>
 
 from rdkit import Chem, DataStructs
 from rdkit.Chem import AllChem
